9f1656c118d0a7eb5b83daf0b22071a55ca2b2a5
WBRK-dev <koertjrweberp@gmail.com>
2024-11-24 20:23:44 +0100

    feat: added switching placeholder

--- resources/js/Pages/Home/Input.svelte
+++ resources/js/Pages/Home/Input.svelte
@@ -1,19 +1,80 @@
 <script>
 
-    import { createEventDispatcher } from "svelte";
+    import {createEventDispatcher, onMount} from "svelte";
 
     export let shortening = false;
     export let shortenedUrl;
 
     const dispatch = createEventDispatcher();
     let value = "";
+    let domainList = [
+        "https://www.google.com/",
+        "https://youtube.com/",
+        "https://www.facebook.com/",
+        "https://www.amazon.com/",
+        "https://www.wikipedia.org/",
+        "https://x.com/",
+        "https://www.instagram.com/",
+        "https://www.reddit.com/",
+        "https://www.linkedin.com/",
+        "https://www.netflix.com/",
+        "https://www.yahoo.com/",
+        "https://www.bing.com/",
+        "https://www.pinterest.com/",
+        "https://www.tumblr.com/",
+        "https://www.spotify.com/",
+        "https://www.tiktok.com/",
+        "https://www.quora.com/",
+        "https://www.ebay.com/",
+        "https://www.github.com/",
+        "https://www.dropbox.com/",
+        "https://www.snapchat.com/",
+        "https://www.airbnb.com/",
+        "https://www.vimeo.com/",
+        "https://www.twitch.tv/",
+        "https://www.stackoverflow.com/",
+        "https://www.salesforce.com/",
+        "https://www.whatsapp.com/",
+        "https://www.apple.com/",
+        "https://www.messenger.com/",
+        "https://www.medium.com/",
+        "https://www.imdb.com/",
+        "https://www.nike.com/",
+        "https://www.hulu.com/",
+        "https://www.revolut.com/",
+        "https://www.bbc.com/",
+        "https://www.nytimes.com/",
+        "https://www.cnn.com/",
+        "https://www.spotify.com/",
+        "https://www.bankofamerica.com/",
+        "https://www.spotify.com/",
+        "https://www.weather.com/",
+        "https://www.chase.com/",
+        "https://www.zillow.com/",
+        "https://www.craigslist.org/",
+        "https://www.skyscanner.net/",
+        "https://www.tesla.com/",
+        "https://www.adobe.com/",
+        "https://www.tripadvisor.com/",
+        "https://www.dell.com/",
+        "https://www.vodafone.com/",
+        "https://www.reddit.com/",
+        "https://www.blogger.com/"
+    ];
+    let domainListIndex = Math.floor(Math.random() * domainList.length);
 
     $: value = shortenedUrl;
+
+    onMount(() => {
+        setInterval(() => {
+            domainListIndex = Math.floor(Math.random() * domainList.length);
+        }, 5000);
+    });
 
 </script>
 
 <div class="wrapper" class:shortening>
-    <input type="text" placeholder="https://" bind:value={value} disabled={shortening}>
+    <input type="text" placeholder={domainList[domainListIndex]} bind:value={value} disabled={shortening}>
     <div class="button-wrapper">
         <button on:click={() => dispatch("shorten", value)}>Shorten</button>
         <button><i class="fi fi-sr-caret-down"></i></button>
@@ -33,6 +94,8 @@
         border-radius: 50rem;
         background-color: #310000;
         backdrop-filter: blur(4px);
+
+        box-shadow: 0 0 5px 5px rgba(0, 0, 0, .1);
 
         overflow: hidden;
         transition: border-color .2s;
